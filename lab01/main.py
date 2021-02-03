efcfeec59abdc87828cc03df47f9fec039b0166a
DIma <dagassacoop@gmail.com>
2021-02-03 13:11:16 +0200

    complete

---
+++ lab01/main.py
@@ -0,0 +1,11 @@
+print('Enter "x":')
+x = int(input())
+print('Enter "y":')
+y = int(input())
+if x > 8:
+    z = 3 + y
+    print('Result: ' + str(z))
+else:
+    z = 9 * x + y
+    print('Result: ' + str(z))
+
